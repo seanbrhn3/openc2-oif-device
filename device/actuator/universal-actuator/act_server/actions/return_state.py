a238041e1819a1cc7d4da60d1da3bfb9029b1256
Sean Brown <seanbrhn3@gmail.com>
2020-04-02 15:32:50 -0400

    testing new changes

--- device/actuator/universal-actuator/act_server/actions/return_state.py
+++ device/actuator/universal-actuator/act_server/actions/return_state.py
@@ -7,4 +7,5 @@ Return_State = Dispatch("return_state")
 
 @Return_State.register
 def default(*extra_args, **extra_kwargs):
+    print("Does it reach theres")
     return dict(status=400,status_text="looks like theres a problem!")
