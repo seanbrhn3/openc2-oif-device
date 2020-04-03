c8c6bed1640fbbf24824b566ec0b824e05afede2
Sean Brown <seanbrhn3@gmail.com>
2020-04-03 15:56:54 +0000

    holding images

--- device/actuator/universal-actuator/act_server/actions/return_state.py
+++ device/actuator/universal-actuator/act_server/actions/return_state.py
@@ -1,10 +1,10 @@
 """
 Return State Target functions
 """
-from ..utils import Dispatch, exceptions
+from .utils import Dispatch, exceptions
 
 Return_State = Dispatch("return_state")
 
 @Return_State.register
 def default(*extra_args, **extra_kwargs):
-    return dict(status=400,status_text="looks like theres a problem!")
+    return dict(status=400,status_text="looks like we're talking! ;)")
